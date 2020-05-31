c2ba634491f730a0c84038f5874ba8db84c94145
codemike@naver.com <codemike@naver.com>
2020-05-31 17:43:05 +0900

    feat - index에 지도 넣을 부분 추가

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+  <div class="map" id="map">
+    지도
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: "PageIndex",
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.map {
+  background-color: greenyellow;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: -1;
+}
+</style>

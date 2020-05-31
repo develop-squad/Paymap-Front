66af26a4312b6f008db5668693c3b0c0f2c36bce
codemike@naver.com <codemike@naver.com>
2020-05-31 21:46:39 +0900

    build - v1.0.3

--- dist/pwa/service-worker.js
+++ dist/pwa/service-worker.js
@@ -19,11 +19,9 @@ importScripts(
 
 workbox.core.setCacheNameDetails({prefix: "paymap-front"});
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to

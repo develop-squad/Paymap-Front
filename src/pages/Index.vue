394582a24ed436805afa910dea849ff9af1256d4
Moon Juhan <35164907+MoonJuhan@users.noreply.github.com>
2020-06-02 22:08:00 +0900

    fix - 불필요한 파일 및 코드 수정

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
+  <div>
     <Map
       v-bind:lat="126.973622"
       v-bind:lng="37.563219"
       v-bind:zoom="17"
     />
-  </q-page>
+  </div>
 </template>
 
 <script>
@@ -13,21 +13,8 @@ import Map from "components/Map"
 
 export default {
   name: "PageIndex",
-
   components: {
     Map
   }
 }
 </script>
-
-<style lang="scss" scoped>
-.map {
-  background-color: greenyellow;
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  z-index: -1;
-}
-</style>

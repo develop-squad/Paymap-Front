cc541e2c0742e3f5bc08c0e0a45bce3355f5c487
Seongbum Seo <sBum.Seo@gmail.com>
2020-06-03 14:15:15 +0900

    feat - 지도의 시작점을 GPS 위치로 설정

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -9,8 +9,7 @@ export default {
   props: {
     lat: Number,
     lng: Number,
-    zoom: Number,
-    mapOptions: Object
+    zoom: Number
   },
   data () {
     return {
@@ -20,26 +19,34 @@ export default {
       map: null
     }
   },
-  watch: {
-    mapOptions: {
-      handler (newValue) {
-        this.map.setOptions(newValue)
-      }
-    }
-  },
 
   mounted () {
-    const options = {
-      center: new window.naver.maps.LatLng(this.lat, this.lng),
-      zoom: this.zoom,
-      zoomControl: true,
-      zoomControlOptions: {
-        style: window.naver.maps.ZoomControlStyle.LARGE,
-        position: window.naver.maps.Position.RIGHT_CENTER
+    this.getGeolocation()
+    this.drawMap()
+  },
+
+  methods: {
+    drawMap () {
+      const options = {
+        center: new window.naver.maps.LatLng(this.lat, this.lng),
+        zoom: this.zoom,
+        zoomControl: true,
+        zoomControlOptions: {
+          style: window.naver.maps.ZoomControlStyle.LARGE,
+          position: window.naver.maps.Position.RIGHT_CENTER
+        }
+      }
+      this.map = new window.naver.maps.Map("map", options)
+    },
+    updatePosition (position) {
+      const center = new window.naver.maps.LatLng(position.coords.latitude, position.coords.longitude)
+      this.map.setCenter(center)
+    },
+    getGeolocation () {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.updatePosition)
       }
     }
-    // let map = new naver.maps.Map('map', initOptions)
-    this.map = new window.naver.maps.Map("map", options)
   }
 }
 </script>
